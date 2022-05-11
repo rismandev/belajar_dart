89ee42a3b24c6ed64f3612b7c1304c2dd07fd061
Risman Abdilah <risman.abdilah@frisidea.com>
2022-05-11 11:11:24 +0700

    Tipe data number

---
+++ bin/number.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  // tipe data integer
+  int numberInteger = 10;
+  print(numberInteger);
+
+  // tipe data double
+  double numberDouble = 10.5;
+  print(numberDouble);
+
+  // tipe data integer/double
+  num number = 20;
+  print(number);
+
+  number = 20.5;
+  print(number);
+}
