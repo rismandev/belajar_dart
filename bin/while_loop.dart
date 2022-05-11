47a0093e4067fd6fdfbfe22fabe4fb17a1ad769a
Risman Abdilah <risman.abdilah@frisidea.com>
2022-05-11 17:20:41 +0700

    Belajar While Loop

---
+++ bin/while_loop.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  // init statement
+  var counter = 1;
+
+  while (counter <= 10) {
+    print("Pengulangan ke-$counter");
+
+    // Post statement
+    counter++;
+  }
+}
