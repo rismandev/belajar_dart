0d018c9e870d976088a8858112a52791c59b6f1e
Risman Abdilah <risman.abdilah@frisidea.com>
2022-05-11 11:05:08 +0700

    Writting comment

---
+++ bin/comment.dart
@@ -0,0 +1,12 @@
+/// this is main function
+/// will be excecuted by dart
+void main(List<String> args) {
+  // This is variable name
+  final String name = "Risman Abdilah";
+
+  /**
+   * This is function to show value from variable name
+   * Add more your comment here
+   */
+  print(name);
+}
