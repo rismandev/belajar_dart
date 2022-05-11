d60b9071113126aec90e94a9f794b3f91335bf99
Risman Abdilah <risman.abdilah@frisidea.com>
2022-05-11 13:50:24 +0700

    Operator perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  var first = 10;
+  var second = 10;
+
+  print(first == second);
+  print(first != second);
+  print(first > second);
+  print(first < second);
+  print(first >= second);
+  print(first <= second);
+
+  var stringFirst = "john";
+  var stringSecond = "doe";
+
+  print(stringFirst == stringSecond);
+  print(stringFirst != stringSecond);
+}
