7a63c113e7acfc742339c447f6088f0944b20bf4
Risman Abdilah <risman.abdilah@frisidea.com>
2022-05-11 11:45:56 +0700

    Tipe data dynamic

---
+++ bin/dynamic.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  // Penulisan menggunakan dynamic
+  dynamic variable = 10;
+  print(variable);
+
+  variable = true;
+  print(variable);
+
+  variable = "Berisi string";
+  print(variable);
+
+  // Penulisan deklarasi var
+  var dynamiVar;
+
+  dynamiVar = 10;
+  print(dynamiVar);
+
+  dynamiVar = "Berubah string";
+  print(dynamiVar);
+
+  dynamiVar = true;
+  print(dynamiVar);
+}
