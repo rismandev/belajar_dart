da8369dacbb8dffd838bfd345fd58899b67e5c55
Risman Abdilah <risman.abdilah@frisidea.com>
2022-05-11 14:00:34 +0700

    Operator penugasan

---
+++ bin/operator_penugasan.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  var a = 10;
+
+  // Tanpa penugasan
+  // a = a + 10;
+
+  print(a);
+
+  // Menggunakan penugasan
+  // a += 10; atau a -= 10;
+
+  // Increment & Decrement
+  // var b = a++; => b = a & a++
+  // var c = ++a; => c = ++a
+}
