90b63111ddd7548c91806ab69851f6af710f52f5
Risman Abdilah <risman.abdilah@frisidea.com>
2022-05-11 17:17:47 +0700

    Belajar For Loop

---
+++ bin/for_loop.dart
@@ -0,0 +1,29 @@
+void main(List<String> args) {
+  // Format pengulangan
+  // for (init statement; kondisi; post statement) {
+  //   block pengulangan
+  // }
+
+  // Pengulangan tanpa henti
+  // for (;;) {
+  //   print("Pengulangan tanpa henti");
+  // }
+
+  // Pengulangan dengan kondisi
+  // var counter = 1;
+  // for (; counter <= 10;) {
+  //   print("Pengulangan ke-$counter");
+  //   counter++;
+  // }
+
+  // Pengulangan dengan kondisi dan init statement
+  // for (var counter = 1; counter <= 10;) {
+  //   print("Pengulangan ke-$counter");
+  //   counter++;
+  // }
+
+  // Pengulangan dengan init statement, kondisi dan post statement
+  for (var counter = 1; counter <= 10; counter++) {
+    print("Pengulangan ke-$counter");
+  }
+}
